99f6f97e3ca4ae4d77d4c42b80bb03e0e6286ee3
Matthieu <matthieufelix@gmail.com>
2016-05-21 21:25:18 +0200

    Remove tokenizer tests

--- main.c
+++ main.c
@@ -3,22 +3,9 @@
 #include "mulisp.h"
 
 
-void tokenize_tester(char * str) {
-    List * ans = tokenize(str);
-
-    while(ans != NULL) {
-        printf(" %s |", (char*)ans->item);
-        ans = ans->next;
-    }
-
-    printf("\n");
-}
-
 
 int main()
 {
-    tokenize_tester("hello");
-    tokenize_tester(" (abc    \"def ghi ::-()\" 3)')   ");
 
     return 0;
 }
